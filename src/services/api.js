8e9517434812a8ecf5c680eeec89002a7e86dbfe
davidfer1112 <davidfernando1112@gmail.com>
2024-11-07 17:48:35 -0500

    Feature: api connection

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 // services/api.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://backpatrones.onrender.com/api';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
